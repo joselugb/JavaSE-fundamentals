e51499675eb3ac86224d017ab16f29ad653c4133
joselugb <joselugb@gmail.com>
2022-02-23 21:51:19 -0500

    primera subida @joselugb

---
+++ src/Variables.java
@@ -0,0 +1,18 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Declarar la variable speed de tipo int (números enteros)
+        int spped;
+
+        // Actualizar el contenido de la variable speed:
+        spped = 10; // si ya habías declarado la variable
+        System.out.println("Variable spped = "+spped);
+
+        // Declarar una variable y asignarle un valor al mismo tiempo:
+        int salary = 1000;
+        System.out.println("Variable salary = "+salary);
+
+        // Crear una variable de tipo String:
+        String eployeeName = "Anahí Salgado";
+        System.out.println("Variable eployeeName = "+eployeeName);
+    }
+}
